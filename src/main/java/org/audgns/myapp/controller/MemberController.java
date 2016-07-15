e1ee129b40913e83a1db31b0a92d6c4cb5e4c53e
audgns <agito12@hanmail.net>
2016-07-15 10:04:17 +0900

    memberController 추가

--- src/main/java/org/audgns/myapp/controller/MemberController.java
+++ src/main/java/org/audgns/myapp/controller/MemberController.java
@@ -1,8 +1,31 @@
 package org.audgns.myapp.controller;
 
+import javax.inject.Inject;
+
+import org.audgns.myapp.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/member/*")
 public class MemberController {
+	@Inject
+	MemberService memberService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String memberLoginGet() {
+		return "/member/login";
+	}
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String memberLoginPost(String userid, String userpw) {
+		logger.info(userid + ", " + userpw);
+//		memberService.loadWithPW(userid, userpw);
+		return "";
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/member/login.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=EUC-KR" isELIgnored="false"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Insert title here</title>
+</head>
+<body>
+<form action="member/login" method="post">
+	아이디 : <input type="text" name="userid"/><br>
+	패스워드 : <input type="password" name="userpw"><br>
+	<button type="submit">로그인</button>
+	<button type="reset">취소</button>
+</form>
+</body>
+</html>
